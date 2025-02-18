d0fbc7e4ba25f2816964b99ba1dbba38e8ec77e9
Faith Kwon <fkwon@smith.edu>
2025-02-17 22:37:47 -0500

    Completed A3

--- Conversation.java
+++ Conversation.java
@@ -1,27 +1,66 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 class Conversation implements Chatbot {
 
   // Attributes 
-
+    String[] responses = {"That's awesome.", 
+                          "Who said that?", 
+                          "Mhm.", 
+                          "You're being really weird right now.", 
+                          "Cool!", 
+                          "Oh my God, you can't say that!", 
+                          "Okay!"};
+    String user_response;
+    String comp_response;
+    int rounds;
+    
+    Scanner input = new Scanner(System.in);
+    Random randomInt = new Random();
+    ArrayList<String> chat = new ArrayList<String>();
+  
   /**
    * Constructor 
    */
   Conversation() {
-    
+
   }
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
+    System.out.println("How many rounds?");
+    rounds = input.nextInt();
 
+    input.nextLine(); // Clears the Scanner buffer
+
+    System.out.println("Hello! What's up?");
+    chat.add("Hello! What's up?");
+
+    for (int i = 0; i < rounds; i++) {
+      user_response = input.nextLine();
+      chat.add(user_response);
+
+      comp_response = respond(user_response);
+      System.out.println(comp_response);
+      chat.add(comp_response);
+    }
+
+    System.out.println("Catch you on the flip side!");
+    chat.add("Catch you on the flip side!");
+
+    System.out.println("\nTRANSCRIPT:");
   }
 
   /**
    * Prints transcript of conversation
    */
   public void printTranscript() {
-
+    for (int i = 0; i < chat.size(); i++) {
+      System.out.println(chat.get(i));
+    }
   }
 
   /**
@@ -30,8 +69,48 @@ class Conversation implements Chatbot {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    String returnString = ""; 
-    return returnString; 
+    /*
+      * I -> you
+      * me -> you
+      * am -> are
+      * you -> I
+      * my -> your
+      * your -> my
+     */
+
+    String returnString = "";
+
+    String[] sentence = inputString.split(" ");
+
+    for (int i = 0; i < sentence.length; i++) {
+      if (i == 0 && sentence[i].equals("I")) {
+        // Making sure that the "I" is the first word in the sentence to capitalize the "You" 
+        returnString += "You ";
+      } if (sentence[i].equals("I")) {
+        returnString += "you ";
+      } else if (sentence[i].equals("me")) {
+        returnString += "you ";
+      } else if (sentence[i].equals("am")) {
+        returnString += "are ";
+      } else if (sentence[i].equals("you") || sentence[i].equals("You")) {
+        returnString += "I ";
+      } else if (sentence[i].equals("my")) {
+        returnString += "your ";
+      } else if (sentence[i].equals("My")) { 
+        // Only adding alternatives if "You" or "My" are capitalized because those are most commonly at the beginning of sentences
+        returnString += "Your ";
+      } else if (sentence[i].equals("your")) {
+        returnString += "my ";
+      } else {
+        returnString += sentence[i] + " ";
+      }
+    }
+
+    if (returnString.equals(inputString + " ")) {
+      returnString = responses[randomInt.nextInt(responses.length)];
+    }
+
+    return returnString;
   }
 
   public static void main(String[] arguments) {
--- reflection.md
+++ reflection.md
@@ -1,7 +1,18 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+    Tanisha Chetty
+
 - Which session(s) of TA / office hours did you attend?
+    7:00-9:00 on Monday
+
 - What are your initial impressions of Java? 
+    This isn't my first time coding in Java (I took AP Computer Science A in high school), so it was nice to get back into the groove of it. Since I'm familiar with Java, the idea-to-code process was much more quick and intuitive to me than it was for Python.
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+    Java is definitely more finicky with specific syntax, such as curly brackets and semicolons. It's also a lot stricter with its data types (i.e. an array has a static number of slots but you can add and remove from an ArrayList). That being said, the two languages share a lot of the key words and ideas, such as for loops, if statements, calling methods, etc.
+    
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    My initial method for the word mirroring was looping through the input string and using .substring to see if the current character plus a certain number of characters was equal to a word (i.e. "me", "you", "I", etc). This was really inefficient and confusing and kept on creating out of bound errors, so I instead used the String.split() method to split up the string into a String array, which ended up being a lot easier to work with. 
+
+    My advice would be to just write down the code, even if you know it'll be inefficient/"bad code", because you can always go back and edit it.
